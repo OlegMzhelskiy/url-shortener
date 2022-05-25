9676069ab64f7f678a7fbc77d7dc7b8ba0377690
OlegMzhelskiy <oleg.mzhelskiy@yandex.ru>
2022-05-25 23:12:56 +0300

    increment #1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,82 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var host = "localhost:8080"
+var urlCollection map[string]string
+
+func main() {
+	urlCollection = make(map[string]string)
+	http.HandleFunc("/", ShortenerHandler)
+	log.Fatal(http.ListenAndServe(host, nil))
+}
+
+func ShortenerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		fmt.Printf("Получен запрос POST %s\n", r.RequestURI)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		shortURL := addToCollection(string(body))
+		w.WriteHeader(http.StatusCreated) //201
+		w.Write([]byte(host + "/" + shortURL))
+	} else if r.Method == http.MethodGet {
+		fmt.Printf("Получен запрос GET %s\n", r.RequestURI)
+		//id := r.URL.Query().Get("id")
+		id := r.URL.Path[1:]
+		if id == "" {
+			http.Error(w, "The query parameter id is missing", http.StatusBadRequest)
+			return
+		}
+		longURL, err := getURLByID(id)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Header().Add("Location", longURL)
+		w.WriteHeader(http.StatusTemporaryRedirect) //307
+	}
+	http.NotFoundHandler()
+}
+
+func addToCollection(longURL string) string {
+	shortURL := generateIdentify(longURL)
+	//Проверим идентификатор на наличие в колекции
+	_, ok := urlCollection[shortURL]
+	if !ok {
+		urlCollection[shortURL] = longURL
+	}
+	return shortURL
+}
+
+func getURLByID(id string) (string, error) {
+	val, ok := urlCollection[id]
+	if !ok {
+		return "", errors.New("not found")
+	}
+	return val, nil
+}
+
+func generateIdentify(s string) string {
+	h := crc32.NewIEEE()
+	h.Write([]byte(s))
+	hashSum := h.Sum32()
+	sHash := strconv.Itoa(int(hashSum))
+	masRune := []rune(sHash)
+	for i, value := range masRune {
+		//masRune[i] = rune(int(value) + 49 + rand.Intn(15))
+		masRune[i] = value + 49
+	}
+	res := string(masRune)
+	return res
+}
