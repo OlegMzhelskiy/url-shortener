4922b75961df9f4fdfd9689b3ac553a076288a26
OlegMzhelskiy <oleg.mzhelskiy@yandex.ru>
2022-06-13 14:25:43 +0300

    increment #5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"url-shortener/cmd/server"
 	"url-shortener/pkg/handler"
 	"url-shortener/storage"
@@ -9,17 +10,17 @@ import (
 
 func main() {
 
-	host := "localhost:8080"
-	baseUrl := "http://" + host
+	//host := "localhost:8080"
+	//baseUrl := "http://" + host
 
-	//host, ok := os.LookupEnv("SERVER_ADDRESS")
-	//if ok == false || host == "" {
-	//	panic("Не задано значение переменной окружения SERVER_ADDRESS")
-	//}
-	//baseUrl, ok := os.LookupEnv("BASE_URL")
-	//if ok == false || baseUrl == "" {
-	//	panic("Не задано значение переменной окружения BASE_URL")
-	//}
+	host, ok := os.LookupEnv("SERVER_ADDRESS")
+	if ok == false || host == "" {
+		panic("Не задано значение переменной окружения SERVER_ADDRESS")
+	}
+	baseUrl, ok := os.LookupEnv("BASE_URL")
+	if ok == false || baseUrl == "" {
+		panic("Не задано значение переменной окружения BASE_URL")
+	}
 
 	strg := storage.NewMemoryRep()
 	handl := handler.NewHandler(strg, baseUrl)
