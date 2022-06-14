d737404010fbccc90716f2091ba4e965c51b6e08
OlegMzhelskiy <oleg.mzhelskiy@yandex.ru>
2022-06-14 20:47:16 +0300

    increment #5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -16,16 +17,22 @@ func main() {
 
 	host, ok := os.LookupEnv("SERVER_ADDRESS")
 	if ok == false || host == "" {
-		panic("Не задано значение переменной окружения SERVER_ADDRESS")
+		//panic("Не задано значение переменной окружения SERVER_ADDRESS")
+		fmt.Println("Не задано значение переменной окружения SERVER_ADDRESS")
+		host = "localhost:8080"
 	}
 	baseUrl, ok := os.LookupEnv("BASE_URL")
 	if ok == false || baseUrl == "" {
-		panic("Не задано значение переменной окружения BASE_URL")
+		//panic("Не задано значение переменной окружения BASE_URL")
+		fmt.Println(("Не задано значение переменной окружения BASE_URL"))
+		baseUrl = "http://" + host
 	}
-	storagePath, _ := os.LookupEnv("FILE_STORAGE_PATH")
-
 	if strings.HasSuffix(baseUrl, "/") == false {
 		baseUrl += "/"
+	}
+	storagePath, ok := os.LookupEnv("FILE_STORAGE_PATH")
+	if ok == false || baseUrl == "" {
+		fmt.Println(("Не задано значение переменной окружения FILE_STORAGE_PATH"))
 	}
 
 	strg := storage.NewMemoryRep(storagePath)
